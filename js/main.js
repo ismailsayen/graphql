c0ead475c30765a6dd08dd570c5e83accd6aa192
Ismail Sayen <ismailsvn02@gmail.com>
2025-05-16 14:00:33 +0100

    handle circle position

--- js/main.js
+++ js/main.js
@@ -3,12 +3,15 @@ import { LoginForm } from "./auth/loginForm.js";
 LoginForm()
 
 
-// addEventListener('mousemove', function MoveCircle(e) {
-//     const circle = document.querySelector(".circle")
-//     circle.style.display = 'flex'
-//     let r = circle.getBoundingClientRect().width / 2
-//     if (e.clientX + r < innerWidth && e.clientY + r < innerHeight) {
-//         circle.style.left = `${e.clientX}px`
-//         circle.style.top = `${e.clientY}px`
-//     }
-// })
+addEventListener('mousemove', function MoveCircle(e) {
+    const circle = document.querySelector(".circle")
+    circle.style.display = 'flex'
+    let r = circle.getBoundingClientRect().width / 2
+    if (e.clientX + r < innerWidth) {
+        circle.style.left = `${e.clientX}px`
+
+    }
+    if (e.clientY + r < innerHeight) {
+        circle.style.top = `${e.clientY}px`
+    }
+})
